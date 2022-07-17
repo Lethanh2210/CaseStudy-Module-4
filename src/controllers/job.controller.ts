8150fa6ca97c46c4d4cae2584f9f5cfc64d78e0e
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-07-17 08:58:54 +0700

    crud

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -2,6 +2,7 @@ import {JobModel} from "../models/job.model";
 import {CategoryModel} from "../models/category.model";
 import {Schema} from "mongoose";
 import multer from "multer";
+
 const upload = require('../middlewares/upload.middleware')
 import path from "path";
 import * as fs from "fs";
@@ -12,11 +13,15 @@ const jobController = {
         const jobs = await JobModel.find();
 
         let user = req.session.passport.user;
-        res.render('home',{jobs:jobs, user:user});
+        res.render('home', {jobs: jobs, user: user});
     },
     renderJobs: async (req, res, next) => {
         const jobs = await JobModel.find();
         res.render('jobs', {jobs: jobs})
+    },
+    renderUpdateJob: async (req, res, next) => {
+        const updateData = await JobModel.findOne({_id: req.params.id}).lean();
+        res.render('updateJob', {data: updateData});
     },
     renderJobDetails: async (req, res, next) => {
 
@@ -44,6 +49,20 @@ const jobController = {
         } catch (e) {
             console.log(e.message);
         }
+    },
+    updateJob: async (req, res, next) => {
+        try {
+            const {companyName,jobName,salary,location,desc,duration,category} = req.body;
+            console.log(req.params.id)
+            const data = await JobModel.findOneAndUpdate({_id: req.params.id},{companyName,jobName,salary,location,desc,duration,category});
+            res.redirect('/cv/jobs')
+        }catch (e) {
+            console.log(e.message);
+        }
+    },
+    deleteJob:async (req, res,next) => {
+        await JobModel.findOneAndRemove({_id: req.params.id}).lean();
+        res.redirect('/cv/jobs');
     }
 }
 
