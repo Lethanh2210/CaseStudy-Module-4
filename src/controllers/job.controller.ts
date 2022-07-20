33c3d2b1b0410587865d76a62c5eb5d6529a36c8
LeThanh <kzxlmdtn888@gmail.com>
2022-07-20 11:49:10 +0700

    done

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -197,7 +197,11 @@ const jobController = {
 
     acceptCV: async (req, res, next) => {
         const authCtrl = new AuthCtrl();
-        await authCtrl.sendMail(req.params.id, req, res);
+        let mail = {
+            email: req.query.email,
+            company: req.query.companyName
+        }
+        await authCtrl.sendMail(mail, req, res);
         res.redirect('/cv');
     }
 }
